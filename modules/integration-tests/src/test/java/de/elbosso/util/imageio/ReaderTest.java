a7874be35504d18916812f0bbd07103ae9da9ae1
Jürgen Key <sgjfsf@hotmail.com>
2018-06-02 07:59:28 +0200

    fixes #1

---
+++ modules/integration-tests/src/test/java/de/elbosso/util/imageio/ReaderTest.java
@@ -0,0 +1,21 @@
+package de.elbosso.util.imageio;
+
+import org.junit.Test;
+
+public class ReaderTest
+{
+
+	@Test
+	public void alwaysSuccess()
+	{
+		java.lang.String[] formatNames=javax.imageio.ImageIO.getReaderFormatNames();
+		for(java.lang.String formatName:formatNames)
+		{
+			java.util.Iterator<javax.imageio.ImageReader> readers=javax.imageio.ImageIO.getImageReadersByFormatName(formatName);
+			while(readers.hasNext())
+			{
+				System.out.println(formatName+" "+readers.next());
+			}
+		}
+	}
+}
